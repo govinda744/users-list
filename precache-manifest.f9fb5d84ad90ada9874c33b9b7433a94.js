e9a67a634bd95c3bb23ae3312698802e6fa58afa
Govinda Bhandari <govinda.bhandari744@gmail.com>
2020-01-07 23:35:23 +0545

    Updates

---
+++ precache-manifest.f9fb5d84ad90ada9874c33b9b7433a94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f65b474c0d66fbb6de080eb37b176bf",
+    "url": "/users-list/index.html"
+  },
+  {
+    "revision": "3d399cb4cc89c0fac7b4",
+    "url": "/users-list/static/css/main.d01b5d0e.chunk.css"
+  },
+  {
+    "revision": "87fd792f8d6a887537b3",
+    "url": "/users-list/static/js/2.daf37fbc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/users-list/static/js/2.daf37fbc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3d399cb4cc89c0fac7b4",
+    "url": "/users-list/static/js/main.a6341cb1.chunk.js"
+  },
+  {
+    "revision": "56cfa94d7bbcc94b2c87",
+    "url": "/users-list/static/js/runtime-main.0a80604b.js"
+  }
+]);
